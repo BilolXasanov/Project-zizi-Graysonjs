b5366065ec3c93d3e163531755273e6e511d3fca
BilolXasanov <xspm24@gmail.com>
2025-02-06 22:10:56 +0500

    two

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-const menuBtn = document.querySelector('.menu-btn'),
-      navigation = document.querySelector(".nav-list");
-
-      menuBtn.addEventListener('click', () => {
-        menuBtn.classList.toggle('active');
-        navigation.classList.toggle('active');
-      });
